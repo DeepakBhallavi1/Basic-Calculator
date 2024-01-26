8f7cb24df656a245be1fc624dd9b098c1c0159a2
Deepak Bhallavi <111530825+DeepakBhallavi1@users.noreply.github.com>
2024-01-26 15:35:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,77 +1,91 @@
-let string = "";
-let btnValue = {
-    "+/-":"mulNeg" , "/":"divide" , "7":"seven" , "8":"eight" ,
-    "9":"nine"     , "4":"four"   , "5":"five"  , "6":"six"   , 
-    "3":"three"    , "2":"two"    , "1":"one"   , "--":"minus",
-    "+":"plus"     , ".":"dot"    , "%":"rem"   , "0":"zero"  ,
-    "=":"equal"    , "AC":"AC"    , "x":"x"     , "X":"X"
+
+const btnValue = { // we will acces class using below value and apply animation
+    "+/-": "mulNeg", "/": "divide", "7": "seven", "8": "eight" ,
+    "9": "nine"    , "4": "four"  , "5": "five" , "6": "six"   , 
+    "3": "three"   , "2": "two"   , "1": "one"  , "--": "minus",
+    "+": "plus"    , ".": "dot"   , "%": "rem"  , "0": "zero"  ,
+    "=": "equal"   , "AC": "AC"   , "DEL": "DEL"    , "X": "X"
 };
 
-let buttons = document.querySelectorAll('.button');
+let output = "";                                    // store input & output
+let inputField = document.querySelector('input');   // shows input and out
+let buttons = document.querySelectorAll('.button'); // all buttons of calculator
 
-Array.from(buttons).forEach((button)=>{
-    let press = button.textContent;
-    let key = btnValue["" + press];
-    button.addEventListener('click',(e)=>{
+// traverse all clicked buttons
+Array.from(buttons).forEach((button) => {
+    let val = button.textContent; // get the text content of button
+    let key = btnValue["" + val]; // create key using text content
 
-        // button animation function
-        buttonAnimation(key);
-
-        let inputField = document.querySelector('input');
-        let txt = press;
-
-        if(txt == 'AC') string = "";  // clears everything
-        else if(txt == '='){ // evaluate expression
-            let newS = "" , n = string.length;
-            if(string != ""){
-                for(let i=0;i<n;i++){
-                    let ch = string.charAt(i);
-                    if((ch >= '0' && ch <= '9') && string.charAt(i-1) == ')') newS = newS + '*' + ch;
-                    else newS = newS + ch;
-                }
-            }
-            string = eval(newS); 
-        }
-        else if(txt == 'x') string = string.slice(0,-1); // remove a input from last
-        else if(txt == '+/-'){
-            // construct number from end of string
-            string = inputField.value;
-            let ch = ""
-            for(let i=string.length-1;i>=0;i--){
-                let d = string.charAt(i);
-                if(d >= '0' && d <= '9') ch = d + ch;
-                else break;
-            }
-            // multiply with -1 
-            if(string != "" && !(isNaN(ch)) && ch != ""){
-                string = string.slice(0,string.length-ch.length);
-                string = string + '(-' + ch + ')';
-            }
-        }
-
-        // adding arithmatic operators 
-        else if((txt == 'X') || (txt == "/") || (txt == '--') || (txt == '+') || (txt == '.') || (txt == '%')){
-            let ch = string.charAt(string.length-1);
-            if((string != "") && (txt == 'X') && (!isNaN(ch) || ch == ')')) string = string + "*";
-            else if((string != "") && (txt == '/') && (!isNaN(ch) || ch == ')')) string = string + "/";
-            else if((txt == '--') && (!isNaN(ch) || ch == ')')) string = string + "-";
-            else if((txt == '+') && (!isNaN(ch) || ch == ')')) string = string + '+';
-            else if((string != "") && (txt == '%') && (!isNaN(ch))) string = string + "%";
-            else if((string != "") && (!isNaN(ch))) string = string + ".";
-        }
-        // adding numbers
-        else string = string + txt; 
-
-        // set result in input field
-        inputField.value = string;
+    // performs events on button click
+    button.addEventListener('click', () => {
+        buttonAnimation(key);   // apply animation
+        calculate(val); // calculate result
     })
 }) 
+
+function calculate(val){
+
+    output = inputField.value;
+    if(val == '='){           // evaluate expression
+        if(output != ""){
+            output = eval(output);     
+        }
+    } 
+    else if(val == 'AC'){     // clears everything
+        output = "";      
+    } 
+    else if(val == 'DEL'){      // remove a input from last
+        output = output.slice(0, -1);
+    }  
+    else if(val == '+/-'){    // construct number from end of string till any operator
+        
+        let lastNumber = "";
+        output = inputField.value;
+
+        let n = output.length;
+        for(let i=n-1; i>=0; i--){
+            let char = output.charAt(i);
+            if(char >= '0' && char <= '9') lastNumber = char + lastNumber;
+            else break;
+        }
+        // add (-number)
+        if((output != "") && lastNumber != ""){
+            output = output.slice(0, n-lastNumber.length);
+            output = output + '(-' + lastNumber + ')';
+        }
+    }
+
+    // adding arithmatic operators with proper checks
+    else if((val == 'X') || (val == "/") || (val == '--') || (val == '+') || (val == '.') || (val == '%')){
+
+        let last = output.charAt(output.length - 1); // access last character of input
+        if(output == "" || (isNaN(last) && last != ')')){ 
+            // do nothing
+        }
+        else if(val == 'X'){
+            output += "*"; // add multplication operator
+        }
+        else if(val == '--'){
+            output += "-"; // add substraction operator
+        } 
+        else{
+            output += val; // adding other operators (/, %, +, .)
+        }
+
+    }
+    // adding numbers
+    else{
+        output += val; 
+    }
+
+    // set result in input field
+    inputField.value = output;
+}
 
 // function for implementing button animations
 function buttonAnimation(key) {
 
     var activeButton = document.querySelector("." + key);
-  
     activeButton.classList.add("pressed");
   
     setTimeout(function() {
